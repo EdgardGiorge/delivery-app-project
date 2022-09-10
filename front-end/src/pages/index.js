0a6c6e1aadd7ccb8d2c339db95eb7f03c62cfe52
EdgardGiorge <edgardgiorge@hotmail.com>
2022-09-10 13:13:07 -0300

    prévia de pages

---
+++ front-end/src/pages/index.js
@@ -0,0 +1,16 @@
+const pages = {
+  Login,
+  Orders,
+  SellerOrders,
+  Register,
+  Products,
+  NotFound,
+  SellerOrderDetails,
+  Admin,
+  Checkout,
+  OrdersDetails,
+  SaleDetails,
+};
+
+export default pages;
+/* Projeto em grupo: Quando o grupo definiu que eu não participaria mais, comecei a adiantar as telas na nova branch do PR individual, agora repassando para a branch oficial "18"  */
