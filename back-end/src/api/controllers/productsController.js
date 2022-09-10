9e438d3d976101d73fb3c58288aaff796f6b19fa
EdgardGiorge <edgardgiorge@hotmail.com>
2022-09-10 14:40:47 -0300

    product controller

---
+++ back-end/src/api/controllers/productsController.js
@@ -0,0 +1,22 @@
+const ProductsService = require('../services/productsService');
+
+const getAllProducts = async (_req, res) => {
+  const products = await ProductsService.getAllProducts();
+  return res.status(200).json(products);
+};
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  const product = await ProductsService.getProductById(id);
+
+  if (!product) return res.status(404).json({ message: 'Not Found.' });
+
+  return res.status(200).json(product);
+};
+
+const createProduct = async (req, res) => {
+  const product = await ProductsService.create(req.body);
+  return res.status(201).json(product);
+};
+
+module.exports = { getAllProducts, getProductById, createProduct };
