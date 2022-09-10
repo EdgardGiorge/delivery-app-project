191f4936be5381fb38edfe672b1cb065cddbf1ae
EdgardGiorge <edgardgiorge@hotmail.com>
2022-09-10 13:20:10 -0300

    browser router

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
+import DeliveryProvider from './context/DeliveryProvider';
+
+/* Projeto em grupo: Quando o grupo definiu que eu não participaria mais, comecei a adiantar as telas na nova branch do PR individual, agora repassando para a branch oficial "18"  */
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <DeliveryProvider>
+        <App />
+      </DeliveryProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
